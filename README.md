9dad83987b734b6d37eeccf662b45f8767edb937
Cody Brunner <rockchalkwushock@icloud.com>
2017-08-12 07:06:27 -0500

    finished :sunglasses:

---
+++ README.md
@@ -0,0 +1,5 @@
+# jQuery Todo List
+
+A simple todo list using jQuery
+
+[Live Demo](https://jquery-todo-list-zbjquwwdfh.now.sh)
---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+/**
+ * NOTE: REMEMBER YOU LOSE THE CONTEXT OF 'THIS' WHEN USING => OVER function!!!
+ */
+
+// Check Off Specific Todos By Clicking
+$('ul').on('click', 'li', function() {
+  $(this).toggleClass('completed')
+})
+
+//Click on X to delete Todo
+$('ul').on('click', 'span', function(e) {
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove()
+  })
+  e.stopPropagation()
+})
+
+$("input[type='text']").keypress(function(e) {
+  if (e.which === 13) {
+    //grabbing new todo text from input
+    var todoText = $(this).val()
+    $(this).val('')
+    //create a new li and add to ul
+    $('ul').append(
+      "<li><span><i class='fa fa-trash'></i></span> " + todoText + '</li>'
+    )
+  }
+})
+
+$('.fa-plus').click(function() {
+  $("input[type='text'").fadeToggle()
+})
